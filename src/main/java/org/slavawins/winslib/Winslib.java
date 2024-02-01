ffb60bfc223050fd4d7e85207aa42af518d80127
slavaWins <zxc7613@mail.ru>
2024-02-01 19:38:50 +0700

    First

---
+++ src/main/java/org/slavawins/winslib/Winslib.java
@@ -0,0 +1,17 @@
+package org.slavawins.winslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Winslib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/slavawins/winslib/example/CustomMath.java
@@ -0,0 +1,7 @@
+package org.slavawins.winslib.example;
+
+public class CustomMath {
+    public static int GetPlus(int val) {
+        return val + 5;
+    }
+}
